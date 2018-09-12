a92c286bcf4980dfc5b640c274b5956bfaa2d198
minomi <5minhominho@gmail.com>
2018-09-12 21:15:08 +0900

    Java Generic

---
+++ Java/Generic/src/obj/Orange.java
@@ -0,0 +1,13 @@
+package obj;
+
+/**
+ * IDE : IntelliJ IDEA
+ * Created by minho on 2018. 9. 12..
+ */
+
+public class Orange {
+    @Override
+    public String toString() {
+        return "I am an orange";
+    }
+}
