3f776a56d7efe2e191025de0479a1613d1a1c83a
minomi <5minhominho@gmail.com>
2018-09-16 13:04:51 +0900

    hello springboot

---
+++ Spring/mastering-spring-mvc4/src/test/java/kr/co/hanbit/mastering/springmvc4/MasteringSpringMvc4ApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.co.hanbit.mastering.springmvc4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MasteringSpringMvc4ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
