839f0ed66e3dcff0e823e94d7f9dd1096028dbcb
minomi <5minhominho@gmail.com>
2018-12-05 22:58:12 +0900

    update TIL

---
+++ DesignPattern/src/main/java/facade/complicated/System01.java
@@ -0,0 +1,8 @@
+package facade.complicated;
+
+/**
+ * IDE : IntelliJ IDEA
+ * Created by minho on 2018-12-05.
+ */
+public class System01 {
+}
---
+++ DesignPattern/src/main/java/facade/main/Application.java
@@ -0,0 +1,8 @@
+package facade.main;
+
+/**
+ * IDE : IntelliJ IDEA
+ * Created by minho on 2018-12-05.
+ */
+public class Application {
+}
